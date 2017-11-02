db6b4ceea9d31051e5bb658d42e2b9120d46114c
am6ku <am6ku@virginia.edu>
2017-11-02 14:03:27 -0400

    Rf improvement

--- Non-Linear Method.R
+++ Non-Linear Method.R
@@ -1,7 +1,7 @@
 library(randomForest)
 library(foreach)
 library(doSNOW)
-
+library(ranger)
 #-------------------------------------------DATA CLEANING------------------------------------------------
 train1 <- read.csv('train.csv', header=T) #reading in the data
 test1 <- read.csv('test.csv', header=T)
@@ -26,8 +26,15 @@ all_data[cat1] <- lapply(all_data[cat1], as.factor)
 num1 <- c(2,4, 15:16, 20:22, 34:52) #create list of num variables
 all_data[num1] <- lapply(all_data[num1], as.numeric)
 
+apply(X= all_data,2,FUN=function(x) length(which(x== -1)))
+#ps_ind_05_cat, ps_car_03_cat, ps_car_05_cat, ps_car_07_cat
+
+#Drop above columns
+
+all_data <- within(all_data, rm(ps_ind_05_cat, ps_car_03_cat, ps_car_05_cat, ps_car_07_cat))
+
 #Impute Mode for factor/Mean for numeric where row value = -1 or missing
-for (i in 2:58){
+for (i in 2:54){
   if (class(all_data[,i]) == 'factor'){
     all_data[,i][all_data[,i] == -1] <- getmode(all_data[,i])
   }
@@ -43,7 +50,7 @@ sum(all_data == -1) #0
 all_data <- droplevels.data.frame(all_data)
 
 #check level count 
-col_levels <- lapply(all_data[,c(3,5:14, 17:19, 23:33, 53:58)], function(x) nlevels(x))
+col_levels <- lapply(all_data, function(x) nlevels(x))
 
 #Check which column has a level count higher than 53(max levels Random forest works with)
 which(col_levels>53) #ps_car_11_cat 
@@ -68,7 +75,7 @@ train_subset <- train[train_subset,]
 X.train = train_subset[, -1]
 Y.train = as.factor(train_subset[, 1])
 
-summary(Y.train)[2]/(summary(Y.train)[1]+summary(Y.train)[1])*100
+summary(Y.train)[2]/(summary(Y.train)[1]+summary(Y.train)[2])*100
 
 #Since the data is so imbalanced, a large m tends to give more acurate predictions.
 #ntrees was fixed at 500 to save computational effort and cross vaidation producedno imporvement 
@@ -79,11 +86,12 @@ cluster = makeCluster(4, type = "SOCK")
 registerDoSNOW(cluster)
 
 set.seed(123)
-rf1 <- foreach(c =c(0.75, 0.80,0.85),ntree = c(100,500,1000), .combine="combine", .multicombine=TRUE,
+rf1 <- foreach(cutoff =c(0.75, 0.80,0.85),ntree = c(100,500,1000), .combine="combine", .multicombine=TRUE,
               .packages='randomForest') %dopar% {
-                randomForest(X.train, Y.train, mtry = 57, ntree = ntree,
-                             importance = TRUE, cutoff =c(c,(1-c)),sampsize = c(382*4,382))
+                randomForest(X.train, Y.train, mtry = 6, ntree = ntree,
+                             importance = TRUE, cutoff =c(cutoff,(1-cutoff)),sampsize = c(382*4,382))
               }
+
 #based on the cross validation, the best classification was produced using a cutoff of 0.80/0.20 and 500 trees
 
 #1s in train data
@@ -91,12 +99,13 @@ summary(as.factor(train[,1]))[2]#21694
 
 #Running that on whole dataset
 
-rf_final <- randomForest(train[,-1], as.factor(train[,1]), mtry = 57, ntree = 500,
-                         importance = TRUE, cutoff =c(0.80,0.20),sampsize = c(21694*4,21694))
+rf_final <- randomForest(train[,-1], as.factor(train[,1]), mtry = 6, ntree = 500,
+                         sampsize = c(21694*5, 21694))
 
-#Make prediction and create submission
-pred <- predict(rf_final, test, type="response")
 
-prediction <- data.frame(test$id,pred)
+#Create Predicitions for submission
+pred <- predict(rf_final, test, type = "prob")
+
+prediction <- data.frame(test$id,pred[,2])
 colnames(prediction) = c("id", "target")
 write.csv(prediction, file = "randomforest.csv", row.names = FALSE)
